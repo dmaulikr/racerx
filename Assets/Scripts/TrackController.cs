d94c7ef300e53eb8b438283c218b4f6abe7fe0e7
Fede <federicotedin@gmail.com>
2016-10-25 15:57:37 -0300

    initial grack generation

---
+++ Assets/Scripts/TrackController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrackController : MonoBehaviour {
+    public GameObject[] trackChunks;
+    public int trackChunkCount = 5;
+
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void GenerateTrack() {
+        if (trackChunks.Length == 0) {
+            Debug.LogError("No track chunks are avaiable.");
+            return;
+        }
+
+        GameObject trackBase = new GameObject("RacingTrack" + Random.Range(0, 10000).ToString());
+        GameObject lastChunk = null;
+
+        for (int i = 0; i < trackChunkCount; i++) {
+            GameObject chunkTemplate = trackChunks[Random.Range(0, trackChunks.Length)];
+
+            GameObject newChunk = Instantiate(chunkTemplate);
+            newChunk.transform.SetParent(trackBase.transform);
+
+            if (lastChunk != null) {
+                MountPoint lastMp = lastChunk.GetComponentInChildren<MountPoint>();
+                if (lastMp == null) {
+                    Debug.LogError("Unable to find mount point in track chunk.");
+                    return;
+                }
+
+                newChunk.transform.position = lastMp.transform.position;
+                Debug.Log(lastMp.transform.position);
+                newChunk.transform.rotation = lastMp.transform.rotation;
+            }
+
+            lastChunk = newChunk;
+        }
+    }
+}
---
+++ Assets/Scripts/TrackControllerEditor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(TrackController))]
+public class TrackControllerEditor : Editor {
+    public override void OnInspectorGUI() {
+        DrawDefaultInspector();
+
+        TrackController trackController = (TrackController)target;
+
+        if (GUILayout.Button("Generate Track")) {
+            trackController.GenerateTrack();
+        }
+    }
+}
