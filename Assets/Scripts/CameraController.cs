deef0f9a4ed1b6c347ba364a07951d04a923f2e2
Jorge Exequiel Gómez <gomezjorge.exequiel@gmail.com>
2016-11-09 01:28:39 -0300

    #Fixed camera bug

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -10,13 +10,25 @@ public class CameraController : MonoBehaviour {
     public Camera camera2;
 
     public void Update() {
-        if (Input.GetKeyDown(KeyCode.Z)) {
-            if (camera1.enabled) {
-                camera1.enabled = false;
-                camera2.enabled = true;
-            } else {
+        if(Input.GetKeyDown(KeyCode.Z)) {
+            if(camera1 == null || camera2 == null) {
+                Camera[] cameras = FindObjectsOfType<Camera>();
+                if (cameras.Length > 0) {
+                    if (cameras[0].name == "Camera1") {
+                        camera1 = cameras[0];
+                        camera2 = cameras[1];
+                    } else {
+                        camera2 = cameras[0];
+                        camera1 = cameras[1];
+                    }
+                }
+            }
+            if (camera2.enabled) {
                 camera1.enabled = true;
                 camera2.enabled = false;
+            } else {
+                camera1.enabled = false;
+                camera2.enabled = true;
             }
         }
     }
